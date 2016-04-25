029090eb7255abb8d530121c3dc5f03e61f46e9d
Brett <brett@applogix.com>
2016-04-25 11:42:52 -0700

    Completed HoneyMakerBee subclass, pseudoclassical pattern

--- src/Bee.js
+++ src/Bee.js
@@ -7,9 +7,3 @@ var Bee = function() {
 
 Bee.prototype = Object.create(Grub.prototype);
 Bee.prototype.constructor = Bee;
-
-// an age property that is set to 5
-// a color property that is set to yellow
-// a food property that is inherited from grub
-// an eat method that is inherited from grub
-// a job property that is set to keep on growing
