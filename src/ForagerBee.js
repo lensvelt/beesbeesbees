04208b2a7b4d9756de396d5a353246c69404e9a9
Brett <brett@applogix.com>
2016-04-25 11:53:33 -0700

    Completed RetiredForagerBee subclass, pseudoclassical style

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -10,4 +10,4 @@ ForagerBee.prototype = Object.create(Bee.prototype);
 ForagerBee.prototype.constructor = ForagerBee;
 ForagerBee.prototype.forage = function(treasure) {
   this.treasureChest.push(treasure);
-}
+};
